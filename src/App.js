801984ff68cb581af5a22c5d73c2eb9bf751cf33
ks-vijai <vijaiks.19cse@kongu.edu>
2023-01-30 22:14:03 +0530

    feat: main-page: add settings button
    Signed-off-by: ks-vijai <vijaiks.19cse@kongu.edu>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 function App() {
-  return <div className="underline text-3xl">App</div>;
+  return (
+    <div>
+      <Router>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent component="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: "yellow", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
